f797750bfabc04ca2806f12f0ab649e6dd30e640
chandanmauryamsfi <mfsi.chandan.maurya@gmail.com>
2021-07-05 17:29:34 +0530

    initial

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 // import Appbar from './AppBar/navbar'
 // import Filterbox from './filterbox';
-import { useState, useEffect , createContext} from "react";
 import bgimg from "./images/karsten-winegeart-sStahKEhT9w-unsplash.jpg"
 import Appbar from "./AppBar/index"
 
